61414b0da1671c2a19868b77d77c2682a57dd5b2
Ethan Chennault <37466516+3than0ls@users.noreply.github.com>
2025-05-18 23:56:05 -0700

    chore: remove inverted index from git and launch script

--- launch.sh
+++ launch.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-export WEBPAGES_DIR="ANALYST/"
-# export WEBPAGES_DIR="DEV/"
-export PARTIAL_INDEX_DIR="partial_indexes/"
 
 # SPECIFY FLAG --no-test TO SKIP UNIT TESTS BEFORE LAUNCH
 if [ "$1" = "--no-test" ]; then
@@ -16,8 +13,3 @@ if [ $? -ne 0 ]; then
 else
     .venv/bin/python3 main.py
 fi
-
-# if [ $? -eq 0 ]; then
-echo "Index vs base size comparison:"
-du -sh "$PARTIAL_INDEX_DIR" "$WEBPAGES_DIR"
-# fi
